8530984d5c7cf86fbfc705a96915e0033adcd89f
Dan McGee <dan@simplebitlabs.com>
2025-04-17 19:20:01 -0500

    Remove unnecessary package

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
 import pluginVue from 'eslint-plugin-vue'
 import vueTsEslintConfig from '@vue/eslint-config-typescript'
 import oxlint from 'eslint-plugin-oxlint'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
 export default [
   {
@@ -17,7 +16,6 @@ export default [
   ...pluginVue.configs['flat/essential'],
   ...vueTsEslintConfig(),
   ...oxlint.configs['flat/recommended'],
-  skipFormatting,
 
   {
     rules: {
